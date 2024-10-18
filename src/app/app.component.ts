cbee499517e35aad6316c2ae1079925f8d5b2c1b
datnv <nguyendat@Nguyens-MacBook-Pro.local>
2024-10-18 11:09:37 +0200

    modify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,7 @@ import {RouterLink, RouterOutlet, RouterModule} from "@angular/router";
   selector: 'app-root',
   standalone: true,
   imports: [HomeComponent, RouterModule, RouterLink, RouterOutlet],
-  template: `
-    <main>
-      <a [routerLink]="['/']">
-        <header class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-        </header>
-      </a>
-      <section class="content">
-        <!--        <app-home></app-home>-->
-        <router-outlet></router-outlet>
-      </section>
-    </main>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
